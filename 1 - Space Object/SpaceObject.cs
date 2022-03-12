be1765c777a049bcdfb68262a2df32f96ce4ab98
larsuib <lju009@uib.no>
2022-03-12 16:59:02 +0100

    Finished task 1 and 2.

---
+++ 1 - Space Object/SpaceObject.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace SpaceSim
+{
+    public class SpaceObject
+    {
+
+        protected String name;
+
+        public SpaceObject(String _name)
+        {
+            name = _name;
+        }
+        public virtual void Draw()
+        {
+            Console.WriteLine(name);
+        }
+    }
+
+    public class Star : SpaceObject
+    {
+        public Star(String _name) : base(_name) { }
+        public override void Draw()
+        {
+            Console.Write("Star  : ");
+            base.Draw();
+        }
+    }
+
+    public class Planet : SpaceObject
+    {
+        public Planet(String _name) : base(_name) { }
+        public override void Draw()
+        {
+            Console.Write("Planet : ");
+            base.Draw();
+        }
+    }
+
+    public class Moon : SpaceObject
+    {
+        public Moon(String _name) : base(_name) { }
+
+        public override void Draw()
+        {
+            Console.Write("Moon  : ");
+            base.Draw();
+        }
+    }
+
+    // omets, asteroids, asteroid belts, and dwarf 
+    // planets.Make sure each of these new objects inherits correctly.
+    public class Comet : SpaceObject
+    {
+        public Comet(String _name) : base(_name) { }
+
+        public override void Draw()
+        {
+            Console.Write("Comet : ");
+            base.Draw();
+        }
+    }
+
+    public class Asteroid : SpaceObject
+    {
+        public Asteroid(String _name) : base(_name) { }
+
+        public override void Draw()
+        {
+            Console.Write("Asteroid : ");
+            base.Draw();
+        }
+    }
+
+    public class AsteroidBelt : SpaceObject
+    {
+        public Asteroid[] Asteroids { get; set; }
+
+        public AsteroidBelt(String _name) : base(_name) { }
+        public AsteroidBelt(String _name, Asteroid[] _asteroids) : base(_name)
+        {
+            Asteroids = _asteroids;
+        }
+
+        public override void Draw()
+        {
+            Console.Write("AsteroidBelt : ");
+            base.Draw();
+        }
+    }
+
+    public class DwarfPlanet : Planet
+    {
+        public DwarfPlanet(String _name) : base(_name) { }
+
+        public override void Draw()
+        {
+            Console.Write("DwarfPlanet : ");
+            base.Draw();
+        }
+    }
+}
+
---
+++ 2 - Space Sim/Program.cs
@@ -0,0 +1,25 @@
+﻿using SpaceSim;
+using System;
+using System.Collections.Generic;
+
+class Astronomy
+{
+    public static void Main()
+    {
+        List<SpaceObject> a = new List<SpaceObject>();
+
+        a.Add(new Star("Sun"));
+        a.Add(new Planet("Mercury"));
+        a.Add(new Planet("Venus"));
+        a.Add(new Planet("Terra"));
+        a.Add(new Moon("The Moon"));
+
+        foreach (SpaceObject s in a)
+        {
+            s.Draw();
+        }
+
+        Console.ReadLine();
+    }
+}
+
