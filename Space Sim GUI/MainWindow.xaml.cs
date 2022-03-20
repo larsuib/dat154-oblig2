3725aa9dc508ff421a4a06f6601c3ff20c1e5c57
Mikael Lindebrekke <239630@stud.hvl.no>
2022-03-20 20:49:35 +0100

    Added name functionality

--- Space Sim GUI/MainWindow.xaml.cs
+++ Space Sim GUI/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Windows.Input;
 
 namespace Space_Sim_GUI
 {
@@ -16,6 +17,8 @@ namespace Space_Sim_GUI
         private DispatcherTimer _dispatchTimer;
         private event Action<double> UpdatePosition;
 
+        private int TextBlockToggle = 0;
+
         #region SpaceObjects
         private Star sun;
         private Planet earth, saturn, mercury, venus, mars, jupiter, uranus, neptune;
@@ -25,19 +28,19 @@ namespace Space_Sim_GUI
         public MainWindow()
         {
             InitializeComponent();
-
+            
             #region SpaceObject Init
             sun = new Star("Sun", 0, 0, 695000, 2332800);
             Moon[] earthMoons = { new Moon("Moon", 384, 27, 1738, 2332800) };
-            earth =         new Planet("Earth", 149600, 365, 6357, 86400, earthMoons);
-            saturn =        new Planet("Saturn", 1429400, 10760, 58232, 38520, new Moon[] {});
-            mercury =       new Planet("Mercury", 57910, 88, 2440, 5067000, new Moon[] {});
-            venus =         new Planet("Venus", 108200, 225, 6052, 10087200, new Moon[] { });
-            mars =          new Planet("Mars", 227940, 687, 3389, 88620, new Moon[] { });
-            jupiter =       new Planet("Jupiter", 778330, 4333, 69911, 35760, new Moon[] { });
-            uranus =        new Planet("Uranus", 2870990, 30685, 25362, 62040, new Moon[] { });
-            neptune =       new Planet("Neptune", 4505300, 60190, 24622, 57960, new Moon[] { });
-            pluto =         new DwarfPlanet("Pluto", 593520, 90550, 1188, 550800, new Moon[] { });
+            earth = new Planet("Earth", 149600, 365, 6357, 86400, earthMoons);
+            saturn = new Planet("Saturn", 1429400, 10760, 58232, 38520, new Moon[] {});
+            mercury = new Planet("Mercury", 57910, 88, 2440, 5067000, new Moon[] {});
+            venus = new Planet("Venus", 108200, 225, 6052, 10087200, new Moon[] { });
+            mars = new Planet("Mars", 227940, 687, 3389, 88620, new Moon[] { });
+            jupiter = new Planet("Jupiter", 778330, 4333, 69911, 35760, new Moon[] { });
+            uranus = new Planet("Uranus", 2870990, 30685, 25362, 62040, new Moon[] { });
+            neptune = new Planet("Neptune", 4505300, 60190, 24622, 57960, new Moon[] { });
+            pluto = new DwarfPlanet("Pluto", 593520, 90550, 1188, 550800, new Moon[] { });
             #endregion
 
             #region SpaceObject UpdatePositionEvent
@@ -61,6 +64,9 @@ namespace Space_Sim_GUI
             };
             _dispatchTimer.Tick += tick;
             _dispatchTimer.Start();
+
+            this.KeyDown += ToggleTextBlocks;
+            
         }
 
         private void tick(object sender, EventArgs eventArgs)
@@ -80,18 +86,7 @@ namespace Space_Sim_GUI
             var centerX = (double) Space.RenderSize.Width / 2;
             var centerY = (double) Space.RenderSize.Height / 2;
 
-            //SetEllipsePositionFromObject(Ellipse_Sun, sun, centerX, centerY);
-            //SetEllipsePositionFromObject(Ellipse_Earth, earth, centerX, centerY);
-            //SetEllipsePositionFromObject(Ellipse_Saturn, saturn, centerX, centerY);
-            //SetEllipsePositionFromObject(Ellipse_Mercury, mercury, centerX, centerY);
-            //SetEllipsePositionFromObject(Ellipse_Venus, venus, centerX, centerY);
-            //SetEllipsePositionFromObject(Ellipse_Mars, mars, centerX, centerY);
-            //SetEllipsePositionFromObject(Ellipse_Jupiter, jupiter, centerX, centerY);
-            //SetEllipsePositionFromObject(Ellipse_Uranus, uranus, centerX, centerY);
-            //SetEllipsePositionFromObject(Ellipse_Neptune, neptune, centerX, centerY);
-            //SetEllipsePositionFromObject(Ellipse_Pluto, pluto, centerX, centerY);
-
-            SetEllipsePositionFromObject(Ellipse_Sun, sun, centerX, centerY);
+            SetGridPositionFromObject(Ellipse_Sun, sun, centerX, centerY);
             SetGridPositionFromObject(Ellipse_Earth, earth, centerX, centerY);
             SetGridPositionFromObject(Ellipse_Saturn, saturn, centerX, centerY);
             SetGridPositionFromObject(Ellipse_Mercury, mercury, centerX, centerY);
@@ -103,15 +98,6 @@ namespace Space_Sim_GUI
             SetGridPositionFromObject(Ellipse_Pluto, pluto, centerX, centerY);
         }
 
-        private void SetEllipsePositionFromObject(Ellipse ellipseObject, SpaceObject spaceObject, double centerX, double centerY)
-        {
-            // Size scaling
-            ellipseObject.Width = SizeScaling(spaceObject);
-            ellipseObject.Height = SizeScaling(spaceObject);
-
-            Canvas.SetLeft(ellipseObject, centerX - ellipseObject.Width / 2 + spaceObject.X);
-            Canvas.SetTop(ellipseObject, centerY - ellipseObject.Height / 2 + spaceObject.Y);
-        }
         private void SetGridPositionFromObject(Grid ellipseObject, SpaceObject spaceObject, double centerX, double centerY)
         {
             // Size scaling
@@ -126,6 +112,43 @@ namespace Space_Sim_GUI
             if (spaceObject.ObjectRadius == 0)
                 return 0;
             return (double) Math.Log(spaceObject.ObjectRadius, 1.22);
+        }
+
+        private void ToggleTextBlocks(Object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+
+                if (TextBlockToggle == 0)
+                {
+                    SunTb.Visibility = Visibility.Visible;
+                    VenusTb.Visibility = Visibility.Visible;
+                    MercuryTb.Visibility = Visibility.Visible;
+                    EarthTb.Visibility = Visibility.Visible;
+                    MarsTb.Visibility = Visibility.Visible;
+                    JupiterTb.Visibility = Visibility.Visible;
+                    SaturnTb.Visibility = Visibility.Visible;
+                    NeptuneTb.Visibility = Visibility.Visible;
+                    UranusTb.Visibility = Visibility.Visible;
+                    PlutoTb.Visibility = Visibility.Visible;
+                    TextBlockToggle = 1;
+                }
+                else
+                {
+                    SunTb.Visibility = Visibility.Hidden;
+                    VenusTb.Visibility = Visibility.Hidden;
+                    MercuryTb.Visibility = Visibility.Hidden;
+                    EarthTb.Visibility = Visibility.Hidden;
+                    MarsTb.Visibility = Visibility.Hidden;
+                    JupiterTb.Visibility = Visibility.Hidden;
+                    SaturnTb.Visibility = Visibility.Hidden;
+                    NeptuneTb.Visibility = Visibility.Hidden;
+                    UranusTb.Visibility = Visibility.Hidden;
+                    PlutoTb.Visibility = Visibility.Hidden;
+                    TextBlockToggle = 0;
+                }
+
+            }
         }
     }
 }
